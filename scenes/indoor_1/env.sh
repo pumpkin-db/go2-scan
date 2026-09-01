6e5f566cbcbf6a0e44602df61271ea4f8f7e56f5
pumpkin-db <pumpkin-db@users.noreply.github.com>
2026-09-01 12:26:04 -0700

    fix(sim): restore indoor spawn height

--- scenes/indoor_1/env.sh
+++ scenes/indoor_1/env.sh
@@ -6,6 +6,6 @@ SCENE_WORLD=$GO2/simulation/worlds/indoor_1.world
 SCENE_GT=$GO2/maps/indoor_1.pcd
 SPAWN_X=-7.5
 SPAWN_Y=0.5
-SPAWN_Z=0.25
+SPAWN_Z=0.32
 SPAWN_YAW=0.0
 export SCENE_WORLD SCENE_GT SPAWN_X SPAWN_Y SPAWN_Z SPAWN_YAW
